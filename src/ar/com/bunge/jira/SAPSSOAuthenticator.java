923d28e1a196d6c04eb08cb163d00d2984459b94
Mariano Capurro <marianocapurro@users.noreply.github.com>
2008-10-10 16:07:46 +0000

    Initial import.

---
+++ src/ar/com/bunge/jira/SAPSSOAuthenticator.java
@@ -0,0 +1,23 @@
+/*
+ * File name: SAPSSOAuthenticator.java
+ * Creation date: Oct 10, 2008 11:55:41 AM
+ * Copyright Mindpool
+ */
+package ar.com.bunge.jira;
+
+/**
+ *
+ * @author <a href="mcapurro@gmail.com">Mariano Capurro</a>
+ * @version 1.0
+ * @since SPM 1.0
+ *
+ */
+public class SAPSSOAuthenticator {
+
+	/**
+	 * 
+	 */
+	public SAPSSOAuthenticator() {
+	}
+
+}
